6952523afeb3ef18ef8e802b224be57f4ac46923
Zeb Wheeler <zebulonwheeler@hotmail.com>
2013-05-06 11:44:52 -0700

    -- refactor several variable names, added comments -- change enemyships to use an array -- added function to spawn enemy ships -- refactored collision detection function -- added function to keep a current enemy ship on screen count

--- first game/first game/Game1.cs
+++ first game/first game/Game1.cs
@@ -21,10 +21,18 @@ namespace first_game
 
         public struct Projectile
         {
-            public Texture2D texture;
-            public Vector2 position;
-            public Vector2 velocity;
-            public bool isAlive;
+            public Texture2D    texture;
+            public Vector2      position;
+            public Vector2      velocity;
+            public bool         is_alive;
+        }
+
+        public struct Enemy_ship
+        {
+            public Texture2D  texture;
+            public Vector2    position;
+            public Vector2    velocity;
+            public bool       is_alive;
         }
 
         public Game1()
@@ -32,6 +40,7 @@ namespace first_game
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
+            //define what screen resolution the game should run in
             graphics.PreferredBackBufferWidth = 1280;
             graphics.PreferredBackBufferHeight = 720;
         }
@@ -52,30 +61,28 @@ namespace first_game
 
         //Global Varibles
 
-        Vector2 player_ship_position;
-        Texture2D player_ship_texture;
+        Vector2     player_ship_position;
+        Texture2D   player_ship_texture;
 
-        Vector2   enemy_ship_position;
-        Texture2D enemy_ship_texture;
-        Vector2   enemy_ship_velocity;
-        bool      enemy_ship_IsAlive;
+        Vector2     background_location;
+        Texture2D   background_texture;
 
-        Vector2 background_location;
-        Texture2D background_texture;
-
-        Projectile single_projectile;
-
-
-        Rectangle viewportRect;
-
+        Rectangle   viewportRect;
         SoundEffect fire_laser_sound;
 
         KeyboardState previousKeyboardState = Keyboard.GetState();
 
-        const int maxProjectiles = 1000;
+        const int   max_projectiles                 = 1000;
+        const int   max_enemy_ships                 = 100;
+        const int   max_enemies_on_screen           = 3;
+        int         current_enemy_ships_on_screen   = 0;
 
         //multiple projectiles
         Projectile[] projectile_array;
+        Enemy_ship[] enemy_ship_array;
+
+        //create a random class instantiation so we can create random numbers
+        static Random random = new Random();
         
 
 
@@ -91,23 +98,10 @@ namespace first_game
             //initialize player ship texture and location
             player_ship_texture = Content.Load<Texture2D>("terran_bc");
 
-            fire_laser_sound = Content.Load<SoundEffect>("sounds\\ship bang");
+            fire_laser_sound = Content.Load<SoundEffect>("sounds\\lazer");
 
             player_ship_position.X = graphics.GraphicsDevice.Viewport.Width / 2;
             player_ship_position.Y = graphics.GraphicsDevice.Viewport.Height / 2;
-
-            //load enemy ship texture, set position and velocity
-
-            enemy_ship_texture = Content.Load<Texture2D>("enemy");
-
-            enemy_ship_position.X = graphics.GraphicsDevice.Viewport.Width;
-            enemy_ship_position.Y = graphics.GraphicsDevice.Viewport.Height / 2;
-
-            enemy_ship_IsAlive = true;
-
-            enemy_ship_velocity.X = -5;
-            enemy_ship_velocity.Y = 0;
-
 
 
             //load background texture and location
@@ -115,21 +109,41 @@ namespace first_game
             background_location.X = 0;
             background_location.Y = 0;
 
-            single_projectile.texture = Content.Load<Texture2D>("SuperShot");
-
-           projectile_array = new Projectile[maxProjectiles];
-
-           for (int i = 0; i < maxProjectiles; i++)
-           {
-               projectile_array[i].texture = Content.Load<Texture2D>("superFireBall");
+            
+            //intialize projectile array
+            projectile_array = new Projectile[max_projectiles];
+            for (int i = 0; i < max_projectiles; i++)
+            {
+                projectile_array[i].texture = Content.Load<Texture2D>("cannonball");
                
-               projectile_array[i].position.X = -10000;
-               projectile_array[i].position.Y = -10000;
+                projectile_array[i].position.X = -10000;
+                projectile_array[i].position.Y = -10000;
 
-               projectile_array[i].velocity.Y = 0;
-               projectile_array[i].velocity.X = 5;
-               projectile_array[i].isAlive = false;
-           }
+                projectile_array[i].velocity.Y = 0;
+                projectile_array[i].velocity.X = 5;
+                projectile_array[i].is_alive = false;
+            }
+
+            enemy_ship_array = new Enemy_ship[max_enemy_ships];
+            for (int i = 0; i < max_enemy_ships; i++)
+            {
+                //assign texture 
+                enemy_ship_array[i].texture = Content.Load<Texture2D>("enemy");
+
+                /*assign position, it does not matter what it is because it will start off with alive = false
+                 * and the position will be set in the function that "spawns" new enemies*/
+                enemy_ship_array[i].position.X = 0;
+                enemy_ship_array[i].position.Y = 0;
+
+                //assign is_alive = false. Enemies will start off "dead", the "spawn" function will set them to alive
+                enemy_ship_array[i].is_alive = false;
+
+                //assign velocity
+                enemy_ship_array[i].velocity.Y = 0;
+                enemy_ship_array[i].velocity.X = -5;
+
+            }
+
 
 
 
@@ -144,23 +158,79 @@ namespace first_game
 
         public void Update_player_projectiles()
         {
-            Rectangle enemy_bounding_box = new Rectangle((int)enemy_ship_position.X,
-                (int)enemy_ship_position.Y, enemy_ship_texture.Width, enemy_ship_texture.Height);
-
-            
-            foreach(Projectile proj in projectile_array)
+           
+            //loop through all enemy ships
+            for (int i = 0; i < max_enemy_ships; i++)
             {
-                if (proj.isAlive)
-                {
-                    Rectangle proj_bounding_box = new Rectangle((int)proj.position.X, (int)proj.position.Y,
-                        proj.texture.Width, proj.texture.Height);
 
-                    if(enemy_bounding_box.Intersects(proj_bounding_box))
+              
+                //create a bounding_box(hitbox) around this particular enemy ship in the array([i])
+                Rectangle enemy_bounding_box = new Rectangle((int)enemy_ship_array[i].position.X,
+                    (int)enemy_ship_array[i].position.Y, enemy_ship_array[i].texture.Width, enemy_ship_array[i].texture.Height);
+
+                //loop through all player projectiles
+                for (int j = 0; j < max_projectiles; j++)
+                {
+                    //create a bounding_Box(hitbox) around this particlar player projectile in the array([j])
+                    Rectangle proj_bounding_box = new Rectangle((int)projectile_array[j].position.X, (int)projectile_array[j].position.Y,
+                            projectile_array[j].texture.Width, projectile_array[j].texture.Height);
+
+                    //check if the two hitboxes intersect each other, if so, it's a hit
+                    if (enemy_bounding_box.Intersects(proj_bounding_box))
                     {
-                        enemy_ship_IsAlive = false;
-                       
+                        //set enemy ship[i] and project[j] is_alive to false
+                        enemy_ship_array[i].is_alive = false;
+                        projectile_array[j].is_alive = false;
+                        current_enemy_ships_on_screen--;
+                        
                     }
                 }
+            }
+        }
+
+        public void spawn_enemy_ship()
+        {
+            
+            
+            if (current_enemy_ships_on_screen < max_enemies_on_screen)
+            {
+                //find a dead enemy ship in the array, then set it's is alive to true and set an random position
+                for (int i = 0; i < max_enemy_ships; i++)
+                {
+                    if (enemy_ship_array[i].is_alive == false)
+                    {
+                        //set X position to the right of the screen
+                        enemy_ship_array[i].position.X = viewportRect.Width;
+                        
+                        /* set Y position to a random position that is still on the screen. Lerp stands for "Linear Interpolation"
+                         * it just takes the random number and converts it into a number that is between the top and bottom of the y-axis */
+                        enemy_ship_array[i].position.Y = MathHelper.Lerp((float)viewportRect.Top - 20,
+                            (float)viewportRect.Bottom -50, (float)random.NextDouble());
+
+                        //don't forget to set enemy ship is_alive to true now!
+                        enemy_ship_array[i].is_alive = true;
+                        
+
+                        
+                        //break out of loop since we only want to create 1 bad guy per call to this function
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void update_enemy_ship_count()
+        {
+            current_enemy_ships_on_screen = 0;
+            for(int i = 0; i < max_enemy_ships; i++)
+            {
+                 Rectangle enemy_bounding_box = new Rectangle((int)enemy_ship_array[i].position.X,
+                    (int)enemy_ship_array[i].position.Y, enemy_ship_array[i].texture.Width, enemy_ship_array[i].texture.Height);
+
+                 if (viewportRect.Intersects(enemy_bounding_box) && enemy_ship_array[i].is_alive)
+                 {
+                     current_enemy_ships_on_screen++;
+                 }
             }
         }
 
@@ -190,22 +260,22 @@ namespace first_game
 
             if (keyboardstate.IsKeyDown(Keys.Up))
             {
-                player_ship_position.Y -= 5;
+                player_ship_position.Y -= 10;
             }
 
             if (keyboardstate.IsKeyDown(Keys.Down))
             {
-                player_ship_position.Y += 5;
+                player_ship_position.Y += 10;
             }
 
             if (keyboardstate.IsKeyDown(Keys.Left))
             {
-                player_ship_position.X -= 5;
+                player_ship_position.X -= 10;
             }
 
             if (keyboardstate.IsKeyDown(Keys.Right))
             {
-                player_ship_position.X += 5;
+                player_ship_position.X += 10;
             }
 
             if (keyboardstate.IsKeyDown(Keys.Enter))
@@ -218,13 +288,13 @@ namespace first_game
             if (keyboardstate.IsKeyDown(Keys.Space) &&
                 previousKeyboardState.IsKeyUp(Keys.Space) )
             {
-                for (int i = 0; i < maxProjectiles; i++)
+                for (int i = 0; i < max_projectiles; i++)
                 {
-                    if (!projectile_array[i].isAlive)
+                    if (!projectile_array[i].is_alive)
                     {
                         projectile_array[i].position.X = player_ship_position.X;
                         projectile_array[i].position.Y = player_ship_position.Y;
-                        projectile_array[i].isAlive = true;
+                        projectile_array[i].is_alive = true;
                         fire_laser_sound.Play();
                         break;
                     }
@@ -236,7 +306,7 @@ namespace first_game
 
             //Update projectiles
 
-            for (int i = 0; i < maxProjectiles; i++)
+            for (int i = 0; i < max_projectiles; i++)
             {
                 projectile_array[i].position.X += projectile_array[i].velocity.X;
                 projectile_array[i].position.Y += projectile_array[i].velocity.Y;
@@ -246,15 +316,23 @@ namespace first_game
             Update_player_projectiles();
 
             //update enemy ship
-            enemy_ship_position.X += enemy_ship_velocity.X;
-            enemy_ship_position.Y += enemy_ship_velocity.Y;
 
-            if (enemy_ship_position.X < 0)
+            for (int i = 0; i < max_enemy_ships; i++)
             {
-                enemy_ship_position.X = graphics.GraphicsDevice.Viewport.Width;
-                enemy_ship_IsAlive = true;
+                enemy_ship_array[i].position.X += enemy_ship_array[i].velocity.X;
+                enemy_ship_array[i].position.X += enemy_ship_array[i].velocity.Y;
+
+                //if the enemy ship is off the screen to the left, set alive = false
+                if (enemy_ship_array[i].position.X < 0 && enemy_ship_array[i].is_alive == true)
+                {
+                    enemy_ship_array[i].is_alive = false;
+                }
             }
 
+            //call our custom spawn enemy function, it will keep max_enemies_on_screen worth of enemies on the screen
+            spawn_enemy_ship();
+            //update the current number of enemyes on the screen using our custom function
+            update_enemy_ship_count();
          
 
             
@@ -282,15 +360,22 @@ namespace first_game
 
             spriteBatch.Draw(background_texture, viewportRect, Color.White);
 
-            for (int i = 0; i < maxProjectiles; i++)
+            for (int i = 0; i < max_projectiles; i++)
             {
-                spriteBatch.Draw(projectile_array[i].texture, projectile_array[i].position, Color.White);
+                if (projectile_array[i].is_alive)
+                {
+                    spriteBatch.Draw(projectile_array[i].texture, projectile_array[i].position, Color.White);
+                }
             }
 
-            if (enemy_ship_IsAlive)
+            for (int i = 0; i < max_enemy_ships; i++)
             {
-                spriteBatch.Draw(enemy_ship_texture, enemy_ship_position, Color.White);
+                if (enemy_ship_array[i].is_alive)
+                {
+                    spriteBatch.Draw(enemy_ship_array[i].texture, enemy_ship_array[i].position, Color.White);
+                }
             }
+
 
             spriteBatch.Draw(player_ship_texture, player_ship_position, Color.White);
 
